6ff78e9de1cf00744c537adb452f8eb8bc50dfe2
castromickael-cpu <castro.mickael@escola.pr.gov.br>
2026-06-26 12:28:43 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function revelarMensagem(){
-document.getElementById("mensagem").innerText = "olá"
+document.getElementById("mensagem").innerText = "VOCE JA ESTÁ DOIDO, repense"
+}
 
+function mostrarNome(){ let nome = document.getElementById ("nome de usuario").value;
+
+document.getElementById("resultado").innerText = "O nome do usuario é: " + nome+ "!";
 
 }
